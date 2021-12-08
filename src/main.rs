b6a15e26670abb9f0a5af34d3a12222483baacd2
K Rhoda <kelsey.rhoda@spruceid.com>
2021-12-08 14:20:04 -0700

    Created data structure needed to support VC storage, relationship querying, and SIWE session management

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,82 @@
 use anyhow::{anyhow, Result};
 use siwe::eip4361::*;
-use std::fs::create_dir_all;
+use std::fs::{create_dir_all, read_to_string};
+use std::io::prelude::*;
 use std::path::Path;
 use walkdir::WalkDir;
 
-fn main() {
-    println!("Hello, world!");
+// TODO: Make a sane default + passed in.
+const home: &str = ".";
+
+fn registry_path() -> &Path {
+    Path::new(home).join(Path::new("registery")).as_path()
 }
 
-fn login(message: Message) -> Result<(User, Vec<String>)> {
-    if !message.valid_now() {
-        return Err(anyhow!("Invalid SIWE Message"));
+fn stat_base(base: &Path) -> Result<()> {
+    let blocked = base.join(Path::new("blocked"));
+    let followees = base.join(Path::new("followees"));
+    let followers = base.join(Path::new("followers"));
+    let nonces = base.join(Path::new("nonces"));
+    if !base.is_dir() {
+        create_dir_all(blocked)?;
+        create_dir_all(followees)?;
+        create_dir_all(followers)?;
+        create_dir_all(nonces)?;
+    } else {
+        if !blocked.is_dir() {
+            create_dir_all(blocked)?;
+        }
+        if !followees.is_dir() {
+            create_dir_all(followees)?;
+        }
+        if !followers.is_dir() {
+            create_dir_all(followers)?;
+        }
+        if !nonces.is_dir() {
+            create_dir_all(nonces)?;
+        }
     };
 
+    Ok(())
+}
+
+fn login(message: Message) -> Result<User> {
+    Ok(User::from_address(validate_session(message)?)?)
+}
+
+fn validate_session(message: Message) -> Result<String> {
     let address = format!(
         "0x{}",
         std::str::from_utf8(&message.address)?.to_lowercase()
     );
 
-    create_dir_all(Path::new(format!("./registry/{}", address)))?;
-    // TODO: Create + Save Session VC here
+    let base = registry_path().join(Path::new(&address)).as_path();
+    stat_base(base)?;
 
-    Ok((
-        User::from_address(address)?,
-        list_users().into_iter().filter(|a| a == &address).collect(),
-    ))
+    if !message.valid_now() {
+        return Err(anyhow!("Invalid SIWE Message"));
+    };
+
+    let next_nonce_path = base
+        .join(Path::new("nonces"))
+        .join(Path::new(format!("{}.txt", &message.nonce)));
+
+    if next_nonce_path.is_file() {
+        let raw_message = read_to_string(next_nonce_path)?;
+        if message.to_string() != raw_message {
+            return Err(anyhow!("Mismatched message"));
+        };
+    } else {
+        let raw_message = message.to_string();
+        let mut f = std::fs::File::create(next_nonce_path)?;
+        f.write_all(raw_message.as_bytes())?;
+    };
+
+    Ok(address)
 }
 
-fn list_users() -> Vec<String> {
-    WalkDir::new("./registry")
+fn list_users(filter_addr: Option<String>) -> Vec<String> {
+    WalkDir::new(registry_path())
         .into_iter()
         .filter_map(|entry| entry.ok())
         .filter(|entry| match entry.metadata() {
@@ -37,54 +85,51 @@ fn list_users() -> Vec<String> {
         })
         .filter_map(|entry| entry.file_name().to_str())
         .map(|s| s.to_string())
+        .filter(|s| Some(s) == filter_addr.as_ref())
         .collect()
 }
 
 struct User {
     pub address: String,
+    pub blocked: Vec<String>,
     pub followed_by: Vec<String>,
     pub follows: Vec<String>,
 }
 
+// TODO: Add test for correct type of VC in correct folder.
+fn valid_vc_addresses(p: &Path) -> Vec<String> {
+    WalkDir::new(p)
+        .into_iter()
+        .filter_map(|entry| entry.ok())
+        .filter(|entry| match entry.metadata() {
+            Err(_) => false,
+            Ok(m) => match m.is_file() {
+                false => false,
+                // TODO: Add VC serialization and validation here.
+                _ => true,
+            },
+        })
+        .filter_map(|entry| entry.file_name().to_str())
+        .map(|s| s.trim_end_matches(".json").to_string())
+        .collect()
+}
+
 impl User {
     fn from_address(address: String) -> Result<Self> {
-        let mut u = User {
+        let base = registry_path().join(Path::new(&address)).as_path();
+        let blocked = base.join(Path::new("blocked"));
+        let followees = base.join(Path::new("followees"));
+        let followers = base.join(Path::new("followers"));
+
+        Ok(User {
             address,
-            followed_by: Vec::new(),
-            follows: Vec::new(),
-        };
-
-        let rels: Vec<String> = WalkDir::new("./followers")
-            .into_iter()
-            .filter_map(|entry| entry.ok())
-            .filter(|entry| match entry.metadata() {
-                Err(_) => false,
-                Ok(m) => m.is_file(),
-            })
-            .filter_map(|entry| entry.file_name().to_str())
-            .map(|s| s.to_string())
-            .collect();
-
-        for rel in rels {
-            let trimmed = rel.trim_end_matches(".json");
-            let v: Vec<&str> = trimmed.split("_").collect();
-            if v.len() != 2 {
-                return Err(anyhow!("Malformed follower data detected, name: {}", rel));
-            };
-
-            let follower = v[0].to_lowercase();
-            let followee = v[1].to_lowercase();
-
-            if follower == u.address || followee == u.address {
-                // TODO: DIDKit check VC here.
-                if follower == u.address {
-                    u.follows.push(followee);
-                } else if followee == u.address {
-                    u.followed_by.push(follower);
-                };
-            }
-        }
-
-        Ok(u)
+            blocked: valid_vc_addresses(&blocked),
+            followed_by: valid_vc_addresses(&followers),
+            follows: valid_vc_addresses(&followees),
+        })
     }
+}
+
+fn main() {
+    println!("Hello, world!");
 }
